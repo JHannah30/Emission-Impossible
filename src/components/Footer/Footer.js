1dbb915ee1ef5ffe76b4fe050c3154777e5722c4
Jamie Hannah <jamiehannah1047@gmail.com>
2022-07-08 15:55:48 +0100

    Added new styling & icons to footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./Footer.css";
+import GitHubIcon from "../../images/github-icon.png"
 
 export default function Footer(props) {
   return (
-    <footer className="footerContainer">
-      <p className="footerHeader">Find us on GitHub:</p>
-      <a href="https://github.com/catherine-butler">Catherine Butler</a>
-      <a href="https://github.com/JHannah30">Jamie Hannah</a>
-      <a href="https://github.com/natpinnock">Natalie Pinnock</a>
-    </footer>
+    <footer className="footer-container">
+      <div className="footer-item" id="footer-title">
+        <p>Find us on GitHub:</p>
+      </div>
+      <div className="footer-item">
+      <a href="https://github.com/catherine-butler" target="_blank" rel="noreferrer"><img src={GitHubIcon}></img></a>
+        <a href="https://github.com/catherine-butler" target="_blank" rel="noreferrer">Catherine Butler</a>
+      </div>
+      <div className="footer-item">
+      <a href="https://github.com/JHannah30" target="_blank" rel="noreferrer"><img src={GitHubIcon}></img></a>
+        <a href="https://github.com/JHannah30" target="_blank" rel="noreferrer">Jamie Hannah</a>
+      </div>
+      <div className="footer-item">
+        <a href="https://github.com/natpinnock" target="_blank" rel="noreferrer"><img src={GitHubIcon}></img></a>
+        <a href="https://github.com/natpinnock" target="_blank" rel="noreferrer">Natalie Pinnock</a>
+      </div>
+    </footer> 
   );
 }
